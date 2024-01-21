d3ac6ce9d40b89e87698254e7a7614531f7dd5e6
ahirner <a.hirner@gmail.com>
2024-01-21 19:45:31 +0100

    refactor pure-system

--- templates/pure-system/flake.nix
+++ templates/pure-system/flake.nix
@@ -1,22 +1,23 @@
 {
   inputs.nix-trickle.url = "github:ahirner/nix-trickle";
-  inputs.darwin.url = "github:lnl7/nix-darwin";
   inputs.nixpkgs.follows = "nix-trickle/nixpkgs";
+  inputs.flake-parts.follows = "nix-trickle/flake-parts";
+  inputs.darwin.url = "github:lnl7/nix-darwin";
 
-  outputs = inputs' @ {
+  outputs = inputs @ {
     self,
+    flake-parts,
     nix-trickle,
     darwin,
     ...
   }: let
     lib = nix-trickle.inputs.nixpkgs.lib;
-    inputs = nix-trickle.inputs // inputs';
-
     # hoists nixpgs with own packages such that those expressions are equivalent:
     # - nix-shell -p <package>           # also needs default.nix with all self.pkgs
     # - nix shell self#<package>
     # - nix shell nix-trickle#<package>  # if not added/overlayed by self
     pureSystem = {
+      #nixpkgs.pkgs = lib.mkForce inputs.nixpkgs.legacyPackages.x86_64-linux;
       # symlink no /etc/nix/inputs/nixpkgs is already in NIX_PATH by mkFlake,
       # therefore overwrite nixpkgs with our source, including default.nix
       environment.etc."nix/inputs/nixpkgs".source = lib.mkForce self.outPath;
@@ -26,49 +27,46 @@
     };
     # minimal config to build a system
     bootableSystem = {
+      nixpkgs.hostPlatform = "x86_64-linux";
       boot.loader.grub.devices = ["nodev"];
       fileSystems."/" = {
         device = "test";
         fsType = "ext4";
       };
     };
-    # expose all of nix-trickle as legacyPackages:
-    # - `nix flake check` doesn't fail eval without --legacy
-    # - `nix shell` finds packages in self not added in self
-    legacyPackages = builtins.mapAttrs (name: value: value.nixpkgs) nix-trickle.pkgs;
   in
-    nix-trickle.lib.mkFlake {
-      # self and inputs are required
-      inherit self inputs legacyPackages;
-
-      # add more default modules
-      extraDefaultModules = [pureSystem nix-trickle.nixosModules.bin-cache];
-
-      # add some own package
-      outputsBuilder = channels: {
-        packages.monad = channels.nixpkgs.writeShellScriptBin "mona" ''
-          echo d
-        '';
-      };
-
-      # example hosts
-      hosts.purely = {
-        modules = [bootableSystem]; # put per-system modules here
-      };
-      hosts.purely-darwin = {
-        system = "x86_64-darwin";
-        output = "darwinConfigurations";
-        builder = darwin.lib.darwinSystem;
-        modules = []; # put per-system modules here
-      };
-
-      # checks for integration tests, can be removed
-      checks = {
-        # breaks or breaks potentially due to: https://github.com/NixOS/nix/issues/4265 https://github.com/NixOS/nix/issues/6806
-        x86_64-darwin.purely-darwin = self.darwinConfigurations.purely-darwin.system;
-        x86_64-linux.purely = self.nixosConfigurations.purely.config.system.build.toplevel;
-        # todo: test nix within configured system
-        # todo: test default nix
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # flakeDefaults needed due to similar error as in https://github.com/numtide/treefmt-nix/issues/78
+      imports = [nix-trickle.flakeModules.flakeDefaults];
+      flake = with nix-trickle.nixosModules; {
+        nixosConfigurations = {
+          purely = inputs.nixpkgs.lib.nixosSystem {
+            modules = [
+              bootableSystem
+              pureSystem
+              bin-cache
+              default
+            ];
+          };
+        };
+        darwinConfigurations = {
+          purely-darwin = darwin.lib.darwinSystem {
+            modules = [
+              {nixpkgs.hostPlatform = "x86_64-darwin";}
+              pureSystem
+              default
+              bin-cache
+            ];
+          };
+        };
+        # checks for integration tests, can be removed for casual use
+        checks = {
+          # breaks or breaks potentially due to: https://github.com/NixOS/nix/issues/4265
+          x86_64-darwin.purely-darwin = self.darwinConfigurations.purely-darwin.system;
+          x86_64-linux.purely = self.nixosConfigurations.purely.config.system.build.toplevel;
+          # todo: test nix within configured system
+          # todo: test default nix
+        };
       };
     };
 }
