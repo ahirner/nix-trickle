963a08fad6d726116d39f212fb0e99b2bd5bfa6c
ahirner <a.hirner@gmail.com>
2024-05-01 12:19:22 +0200

    9 checks failed

--- overlays/datafusion-cli.nix
+++ overlays/datafusion-cli.nix
@@ -9,14 +9,27 @@ final: prev: {
         rev = version;
         hash = "sha256-Gl/fcT+5JsS+5DokVaxPdja8mdCjClHq5QnL5b8MfJk=";
       };
+      cargoDeps = prev.rustPlatform.importCargoLock {
+        lockFile = "${src}/datafusion-cli/Cargo.lock";
+      };
       buildInputs =
         old.buildInputs
         ++ prev.lib.optionals prev.stdenv.isDarwin [
           prev.darwin.apple_sdk.frameworks.SystemConfiguration
         ];
-      cargoDeps = prev.rustPlatform.importCargoLock {
-        lockFile = "${src}/datafusion-cli/Cargo.lock";
-      };
+      checkFlags =
+        old.checkFlags # didn't check to prune old
+        ++ [
+          "--skip=catalog::tests::query_gs_location_test"
+          "--skip=catalog::tests::query_http_location_test"
+          "--skip=catalog::tests::query_s3_location_test"
+          "--skip=exec::tests::copy_to_external_object_store_test"
+          "--skip=exec::tests::copy_to_object_store_table_s3"
+          "--skip=exec::tests::create_object_store_table_cos"
+          "--skip=exec::tests::create_object_store_table_http"
+          "--skip=exec::tests::create_object_store_table_oss"
+          "--skip=exec::tests::create_object_store_table_s3"
+        ];
     }
   );
 }
