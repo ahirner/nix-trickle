c4ee2b86680d40f2e3016bccfe633ef1e134fa81
ahirner <a.hirner@gmail.com>
2024-06-04 19:50:05 +0200

    Revert "no grafana"
    This reverts commit d3cb11102b0c7821aca36bedb77e002703eb4def.

--- templates/devShell/flake.nix
+++ templates/devShell/flake.nix
@@ -23,7 +23,7 @@
         ...
       }: let
         checks = with pkgs; {
-          inherit micromamba vector;
+          inherit micromamba grafana vector;
           helix = helix.packages.${system}.default;
         };
       in {
