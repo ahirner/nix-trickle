ade2fc52a4786f2415e1805df3a58e23bf702f7d
ahirner <a.hirner@gmail.com>
2024-06-28 16:59:47 +0200

    shellcheck

--- tests/templates.sh
+++ tests/templates.sh
@@ -3,17 +3,17 @@
 
 clone() {
   t=$1
-  rm $t/* || true
-  mkdir -p $t
+  rm "$t"/* || true
+  mkdir -p "$t"
 
-  (cd $t
-  nix flake init -t ../..#$t
+  (cd "$t"
+  nix flake init -t ../..#"$t"
   git init
-  git add *)
+  git add ./*)
 }
 
 check() {
-  (cd $1
+  (cd "$1"
   nix flake update --override-input nix-trickle ../..
   nix flake metadata
   nix flake show
@@ -22,10 +22,10 @@ check() {
 
 check-pkg-default-overlay() {
   tem=$1; pkg=$2
-  (cd $tem
-   nix build ."#"$pkg && flake=$(readlink result)
+  (cd "$tem"
+   nix build ."#""$pkg" && flake=$(readlink result)
    rm result
-   nix-build -A $pkg && defex=$(readlink result)
+   nix-build -A "$pkg" && defex=$(readlink result)
    echo "flake $flake"
    echo "defex $defex"
    if [ "$flake" != "$defex" ]; then exit 1; fi
@@ -34,9 +34,9 @@ check-pkg-default-overlay() {
 
 check-pkg-default() {
   tem=$1; pkg=$2
-  (cd $tem
-   nix build ."#"$pkg && flake=$(readlink result)
-   nix-build -A $pkg && defex=$(readlink result)
+  (cd "$tem"
+   nix build ."#""$pkg" && flake=$(readlink result)
+   nix-build -A "$pkg" && defex=$(readlink result)
    echo "flake $flake"
    echo "defex $defex"
    if [ "$flake" != "$defex" ]; then exit 1; fi
@@ -46,10 +46,10 @@ check-pkg-default() {
 check-pkg-overlayed() {
   tem=$1; pkg=$2
   echo "only succeeds on hosts where nixpkgs!=nix-trickle in NIX_PATH and $pkg differs"
-  (pushd $tem
-   nix build ."#"$pkg && flake=$(readlink result)
+  (pushd "$tem"
+   nix build ."#""$pkg" && flake=$(readlink result)
    popd
-   nix-build '<nixpkgs>' -A $pkg && host=$(readlink result)
+   nix-build '<nixpkgs>' -A "$pkg" && host=$(readlink result)
    echo "flake $flake"
    echo "host  $host"
    if [ "$flake" == "$host" ]; then exit 1; fi
@@ -59,7 +59,7 @@ check-pkg-overlayed() {
 set -e
 cmd=$1
 if [ "$cmd" = "check" ] || [ "$cmd" = "clone" ]; then
-  for arg in "${@:2}"; do $cmd $arg; done
+  for arg in "${@:2}"; do $cmd "$arg"; done
 else
-  for arg in "${@:3}"; do $cmd $2 $arg; done
+  for arg in "${@:3}"; do $cmd "$2" "$arg"; done
 fi
