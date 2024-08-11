6d930334654c79bf29f27cbe0dd6731ff3423e3d
ahirner <a.hirner@gmail.com>
2024-08-11 10:36:27 +0200

    datafusion-cli 40.0.0

--- overlays/datafusion-cli.nix
+++ overlays/datafusion-cli.nix
@@ -1,13 +1,13 @@
 final: prev: {
   datafusion-cli = prev.datafusion-cli.overrideAttrs (
     old: rec {
-      version = "37.0.0";
+      version = "40.0.0";
       src = prev.fetchFromGitHub {
         name = "datafusion-cli-source";
         owner = "apache";
         repo = "arrow-datafusion";
         rev = version;
-        hash = "sha256-Gl/fcT+5JsS+5DokVaxPdja8mdCjClHq5QnL5b8MfJk=";
+        hash = "sha256-JXq0REmZB3ruxCsSaKjY46NyFuKYeh0vlYqjtpl5SK4=";
       };
       cargoDeps = prev.rustPlatform.importCargoLock {
         lockFile = "${src}/datafusion-cli/Cargo.lock";
