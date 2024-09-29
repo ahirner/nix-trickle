8bd9c7761dda91233bec4a26defa9ef28e34fb7d
ahirner <a.hirner@gmail.com>
2024-09-29 17:07:27 +0200

    sqruff 0.18.2 w rust 1.81 stable

--- overlays/sqruff.nix
+++ overlays/sqruff.nix
@@ -1,21 +1,21 @@
 final: prev: let
   inherit (prev) lib fetchFromGitHub;
-  rustPlatformNightly = prev.makeRustPlatform {
-    cargo = prev.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
-    rustc = prev.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+  rustPlatform = prev.makeRustPlatform {
+    cargo = prev.rust-bin.stable.latest.default;
+    rustc = prev.rust-bin.stable.latest.default;
   };
 in {
-  sqruff = rustPlatformNightly.buildRustPackage rec {
+  sqruff = rustPlatform.buildRustPackage rec {
     pname = "sqruff";
-    version = "0.14.0";
+    version = "0.18.2";
 
     src = fetchFromGitHub {
       owner = "quarylabs";
       repo = pname;
       rev = "v${version}";
-      hash = "sha256-S/UmGAThlhKFh58MOD7v5+nPOot+Jh5X2oFdrXYQfkE=";
+      hash = "sha256-LLRBnyRJCXwCTbollsFtl+w5/Urq36xTcYNzpQF/f+k=";
     };
-    cargoHash = "sha256-aofsUUOOmK40dCdDtsHh0ARVCJRr754b5P6sogXjPZA=";
+    cargoHash = "sha256-ps6+yCbhU5LwaG6pzEclHn0EXMuA3wtr8XT7xjVvpSQ=";
     # tests/ui.rs refers to ../../target/release/sqruff which doesn't exist in nix
     doCheck = false;
 
