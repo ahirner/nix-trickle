eeefc73f5a470ea0e0f482c3b9e99c2616ff4433
ahirner <a.hirner@gmail.com>
2024-11-10 07:20:41 +0100

    pspg 5.8.7-patched

--- overlays/pspg.nix
+++ overlays/pspg.nix
@@ -1,12 +1,12 @@
 final: prev: {
   # recent and updating querystream from file
   pspg = prev.pspg.overrideAttrs (old: {
-    version = "5.8.0-patched";
+    version = "5.8.7-patched";
     src = prev.fetchFromGitHub {
       owner = "okbob";
       repo = old.pname;
-      rev = "1e0028d9d0c17b0956f844205211cf6d9a92b456";
-      hash = "sha256-apcvYonFl8+vJ7CPBs8f1UA+bD63TchA5++Td+RNQHY=";
+      rev = "refs/tags/5.8.7";
+      hash = "sha256-SE+62EODKWcKFpMMbWDw+Dp5b2D/XKbMFiJiD/ObrhU=";
     };
     patches = prev.patches or [] ++ [../patches/pspg.patch];
     meta = old.meta // {mainProgram = old.pname;};
