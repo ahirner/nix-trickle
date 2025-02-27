6ff1eafef239a23e3f3f77a6d0b2b7b4cf4c08ec
ahirner <a.hirner@gmail.com>
2025-02-27 07:00:33 +0100

    sqruff 0.25.10

--- overlays/sqruff.nix
+++ overlays/sqruff.nix
@@ -7,15 +7,17 @@ final: prev: let
 in {
   sqruff = rustPlatform.buildRustPackage rec {
     pname = "sqruff";
-    version = "0.20.0";
+    version = "0.25.10";
 
     src = fetchFromGitHub {
       owner = "quarylabs";
       repo = pname;
       rev = "v${version}";
-      hash = "sha256-lgNRvd7rczuR4sk50sspJsMEDq1TgbIUYNiMs1MIiJk=";
+      hash = "sha256-tCx+AeTLssXYXKBa7xwuddM8TPiIU6qaxrXeMTqrE0g=";
     };
-    cargoHash = "sha256-i8RehIMOscf5gbkIrRhI3J05h2U2GOQeLhYsNUkoxBk=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-I7DiJqVKHtSxBE2C9/oKOmogG56a7tAdg02YjQ0MbTI=";
+
     # tests/ui.rs refers to ../../target/release/sqruff which doesn't exist in nix
     doCheck = false;
 
