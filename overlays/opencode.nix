bb43ae39dae6a8e7a4403decd2f1dd4ec6d23aff
ahirner <a.hirner@gmail.com>
2025-07-24 18:14:03 +0200

    opencode 0.3.61

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -16,19 +16,19 @@ in {
       "aarch64-darwin" = "";
       "aarch64-linux" = "";
     };
-    version = "0.3.57";
+    version = "0.3.61";
     src = pkgs.fetchFromGitHub {
       owner = "sst";
       repo = "opencode";
       tag = "v${version}";
-      hash = "sha256-/YIfLYzCprgTedjAeSFuck4aix/Drx7iwG7rCC2MfPE=";
+      hash = "sha256-0N4VsGa3l8IWy8YMCuDQJoxWxTQtXQBt0scyPPiRwvI=";
     };
   in {
     inherit version src;
     tui = prev.tui.overrideAttrs (prev: {
       inherit (finalAttrs) version;
       src = "${finalAttrs.src}/packages/tui";
-      vendorHash = "sha256-8OIPFa+bl1If55YZtacyOZOqMLslbMyO9Hx0HOzmrA0=";
+      vendorHash = "sha256-gvWD8ILnA5NxGpiNMcNFUI6YVLMeRGz45pDk0G5zBjc=";
     });
     node_modules = prev.node_modules.overrideAttrs (prev: {
       inherit (finalAttrs) version src;
