f85ea20947a45c6c5fed90577aa9fe330c9e4274
ahirner <a.hirner@gmail.com>
2025-10-15 20:26:21 +0200

    nixos-unstable

--- overlays/pspg.nix
+++ overlays/pspg.nix
@@ -1,14 +1,8 @@
 final: prev: {
   # recent and updating querystream from file
   pspg = prev.pspg.overrideAttrs (old: {
-    version = "5.8.7-patched";
-    src = prev.fetchFromGitHub {
-      owner = "okbob";
-      repo = old.pname;
-      rev = "refs/tags/5.8.7";
-      hash = "sha256-SE+62EODKWcKFpMMbWDw+Dp5b2D/XKbMFiJiD/ObrhU=";
-    };
+    inherit (old) src meta;
+    version = "5.8.12-patched";
     patches = prev.patches or [] ++ [../patches/pspg.patch];
-    meta = old.meta // {mainProgram = old.pname;};
   });
 }
