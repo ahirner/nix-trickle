6948b713b69bde9069d77df38265020261cb5e1e
ahirner <a.hirner@gmail.com>
2025-12-13 19:39:17 +0100

    builds with dontCheckRuntimeDeps = true but breaks on pandas np.product
    https://github.com/ranaroussi/quantstats/issues/365

--- overlays/superset.nix
+++ overlays/superset.nix
@@ -11,7 +11,6 @@ final: prev: let
     hash = "sha256-lHHbSBSPT8UUAYmlpDHuwdhyy8u4/emydoPa9G8uXZ8=";
   };
 
-
   sqlalchemy-utils = python3.pkgs.buildPythonPackage rec {
     pname = "SQLAlchemy-Utils";
     version = "0.38.3";
@@ -21,9 +20,9 @@ final: prev: let
       hash = "sha256-n5r7pgekBFXPcDrfqYRlhL8mFooMWmCnAGO3DWUFH00=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
-    dependencies = [ python3.pkgs.sqlalchemy_1_4 ];
+    dependencies = [python3.pkgs.sqlalchemy_1_4];
   };
 
   prison = python3.pkgs.buildPythonPackage rec {
@@ -35,9 +34,9 @@ final: prev: let
       hash = "sha256-5s1yQESvyxqKaTQMrS8eMVGlg5/TqAJ/0TV1ceeXxZk=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
-    dependencies = with python3.pkgs; [ six ];
+    dependencies = with python3.pkgs; [six];
   };
 
   flask-login = python3.pkgs.buildPythonPackage rec {
@@ -49,9 +48,9 @@ final: prev: let
       hash = "sha256-XiPRSmB+8SgGxplZC4nQ8ODWe67sWZ11lHv5wUczAzM=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
-    dependencies = with python3.pkgs; [ flask ];
+    dependencies = with python3.pkgs; [flask];
   };
 
   flask-sqlalchemy = python3.pkgs.buildPythonPackage rec {
@@ -63,7 +62,7 @@ final: prev: let
       hash = "sha256-xXZeWMoUVAG1IQbA9GF4VpJDxdolVWviwjHsxghnxbE=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel flit-core ];
+    build-system = with python3.pkgs; [setuptools wheel flit-core];
     doCheck = false;
     dependencies = with python3.pkgs; [
       flask
@@ -80,7 +79,7 @@ final: prev: let
       hash = "sha256-NSOndDkO8MHA98cIp1GYCcU5bPYIcg8U9Vw290/1u+w=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
     dependencies = with python3.pkgs; [
       marshmallow
@@ -97,7 +96,7 @@ final: prev: let
       hash = "sha256-bD3HdeZe/CziwVemWst3bWNMuBRZj0BkaavvAK4/Y1w=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel flit-core ];
+    build-system = with python3.pkgs; [setuptools wheel flit-core];
     doCheck = false;
   };
 
@@ -110,7 +109,7 @@ final: prev: let
       hash = "sha256-BaIfz07aGl7dvSkYkK3iP8Xdqmu5jy7iPS04TtFPCG0=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
   };
 
@@ -125,16 +124,16 @@ final: prev: let
       hash = "sha256-4cD+xthzYCJ7XhoRNZOBG6NrkPMAv12aAEDnmTey76c=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel setuptools-scm ];
+    build-system = with python3.pkgs; [setuptools wheel setuptools-scm];
     doCheck = false;
     dependencies = with python3.pkgs; [
-        # minimal deps for now
-        apsw
-        requests
-        requests-cache
-        sqlalchemy_1_4
-        python-dateutil
-        greenlet
+      # minimal deps for now
+      apsw
+      requests
+      requests-cache
+      sqlalchemy_1_4
+      python-dateutil
+      greenlet
     ];
   };
 
@@ -147,9 +146,46 @@ final: prev: let
       hash = "sha256-eCcoUmo5Y+nNhZSIlBhb/1UjPz0GgVUXVGXOzsCdIjQ=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
-    dependencies = with python3.pkgs; [ wtforms six ];
+    dependencies = with python3.pkgs; [wtforms six];
+  };
+
+  alembic = python3.pkgs.buildPythonPackage rec {
+    pname = "alembic";
+    version = "1.13.3";
+    src = prev.fetchPypi {
+      pname = "alembic";
+      inherit version;
+      hash = "sha256-IDUDEXQVVh4gOqFFQXQGQ6YR9kFRfwIJ/K5j6foJ8aI=";
+    };
+    pyproject = true;
+    build-system = with python3.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python3.pkgs; [
+      mako
+      python-dateutil
+      sqlalchemy_1_4
+      typing-extensions
+    ];
+  };
+
+  flask-migrate = python3.pkgs.buildPythonPackage rec {
+    pname = "Flask-Migrate";
+    version = "4.0.7";
+    src = prev.fetchPypi {
+      pname = "Flask-Migrate";
+      inherit version;
+      hash = "sha256-3/fdJRE8IQsGmvKA6nE7iD84QMHjRVJ0dF1zVXeMhiI=";
+    };
+    pyproject = true;
+    build-system = with python3.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python3.pkgs; [
+      alembic
+      flask
+      flask-sqlalchemy
+    ];
   };
 
   flask-appbuilder = python3.pkgs.buildPythonPackage rec {
@@ -161,25 +197,27 @@ final: prev: let
       hash = "sha256-9Xe5gqGuQLwhMjjO25PDnGfPIZmqHgBuCH6hs1B9VFA=";
     };
     pyproject = true;
-    build-system = with python3.pkgs; [ setuptools wheel ];
+    build-system = with python3.pkgs; [setuptools wheel];
     doCheck = false;
-    dependencies = [ flask-login prison sqlalchemy-utils python3.pkgs.sqlalchemy_1_4 ] ++ (with python3.pkgs; [
-      apispec
-      colorama
-      click
-      email-validator
-      flask
-      flask-babel
-      flask-limiter
-      flask-sqlalchemy
-      flask-wtf
-      flask-jwt-extended
-      jsonschema
-      marshmallow
-      marshmallow-sqlalchemy
-      python-dateutil
-      pyjwt
-    ]);
+    dependencies =
+      [flask-login prison sqlalchemy-utils python3.pkgs.sqlalchemy_1_4]
+      ++ (with python3.pkgs; [
+        apispec
+        colorama
+        click
+        email-validator
+        flask
+        flask-babel
+        flask-limiter
+        flask-sqlalchemy
+        flask-wtf
+        flask-jwt-extended
+        jsonschema
+        marshmallow
+        marshmallow-sqlalchemy
+        python-dateutil
+        pyjwt
+      ]);
   };
 in {
   superset = python3.pkgs.buildPythonApplication {
@@ -188,64 +226,64 @@ in {
     pyproject = true;
     build-system = with python3.pkgs; [setuptools wheel];
 
-    dependencies = [ flask-appbuilder flask-login hashids python-geohash shillelagh sqlalchemy-utils python3.pkgs.sqlalchemy_1_4 wtforms-json ] ++ (with python3.pkgs; [
-      backoff
-      celery
-      click
-      click-option-group
-      colorama
-      flask-cors
-      croniter
-      cron-descriptor
-      cryptography
-      deprecation
-      flask
-      flask-caching
-      flask-compress
-      flask-talisman
-      flask-migrate
-      flask-session
-      flask-wtf
-      geopy
-      greenlet
-      gunicorn
-      holidays
-      humanize
-      isodate
-      jsonpath-ng
-      mako
-      markdown
-      marshmallow
-      msgpack
-      nh3
-      numpy
-      pandas
-      bottleneck
-      parsedatetime
-      paramiko
-      pgsanity
-      pillow
-      polyline
-      pydantic
-      pyparsing
-      python-dateutil
-      python-dotenv
-      pyarrow
-      pyyaml
-      pyjwt
-      redis
-      selenium
-      sshtunnel
-      simplejson
-      slack-sdk
-      sqlglot
-      tabulate
-      typing-extensions
-      watchdog
-      wtforms
-      xlsxwriter
-    ]);
-
+    dependencies =
+      [flask-appbuilder flask-migrate flask-login hashids python-geohash shillelagh sqlalchemy-utils python3.pkgs.sqlalchemy_1_4 wtforms-json]
+      ++ (with python3.pkgs; [
+        backoff
+        celery
+        click
+        click-option-group
+        colorama
+        flask-cors
+        croniter
+        cron-descriptor
+        cryptography
+        deprecation
+        flask
+        flask-caching
+        flask-compress
+        flask-talisman
+        flask-session
+        flask-wtf
+        geopy
+        greenlet
+        gunicorn
+        holidays
+        humanize
+        isodate
+        jsonpath-ng
+        mako
+        markdown
+        marshmallow
+        msgpack
+        nh3
+        numpy
+        pandas
+        bottleneck
+        parsedatetime
+        paramiko
+        pgsanity
+        pillow
+        polyline
+        pydantic
+        pyparsing
+        python-dateutil
+        python-dotenv
+        pyarrow
+        pyyaml
+        pyjwt
+        redis
+        selenium
+        sshtunnel
+        simplejson
+        slack-sdk
+        sqlglot
+        tabulate
+        typing-extensions
+        watchdog
+        wtforms
+        xlsxwriter
+      ]);
 
     meta = {
       description = "Data Visualization and Exploration Platform";
@@ -254,5 +292,7 @@ in {
       license = lib.licenses.asl20;
       mainProgram = "superset";
     };
+
+    dontCheckRuntimeDeps = true;
   };
 }
