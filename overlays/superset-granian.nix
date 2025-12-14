732f430c9bb63f093ffd676874ab82604727970c
Alexander Hirner <6055037+ahirner@users.noreply.github.com>
2025-12-14 18:25:29 +0100

    Feature/apache superset (#108)
    * got started
    
    * progress.. dupcliate sqlalchemy 2 in tree
    
    * only pythonRuntimeDepsCheck fails
    
      - flask-cors<7.0,>=4.0.2 not satisfied by version 0.0.1
      - cryptography<45.0.0,>=42.0.4 not satisfied by version 46.0.2
      - flask<3.0.0,>=2.2.5 not satisfied by version 3.1.2
      - flask-migrate<4.0,>=3.1.0 not satisfied by version 4.1.0
      - greenlet<=3.1.1,>=3.0.3 not satisfied by version 3.2.3
      - msgpack<1.1,>=1.0.0 not satisfied by version 1.1.1
      - numpy<2.3,>1.23.5 not satisfied by version 2.3.4
      - pandas<2.2,>=2.1.4 not satisfied by version 2.3.1
      - pillow<12,>=11.0.0 not satisfied by version 12.0.0
      - pyarrow<19,>=16.1.0 not satisfied by version 20.0.0
      - redis<5.0,>=4.6.0 not satisfied by version 6.2.0
      - shillelagh<2.0,>=1.4.3 not satisfied by version 1.3.1
      - xlsxwriter<3.1,>=3.0.7 not satisfied by version 3.2.5
    
    * builds with dontCheckRuntimeDeps = true but breaks on pandas np.product
    
    https://github.com/ranaroussi/quantstats/issues/365
    
    * fix numpy.product -> prod and relax dependencies
    
    * fix missing google dep in shillelagh
    
    * dontStrip saves build time
    
    * sort deps
    
    * dependency hygiene
    
    * make use of pypi fronted static assets add tests incl integration
    
    * modularize
    
    * enable psql support
    
    * enable datafusion+flightsql w/ flightsql-dbapi 0.2.2
    
    * superset.granian passthru
    
    * superset-granian docker image
    
    * add superset
    
    * thx gemini

---
+++ overlays/superset-granian.nix
@@ -0,0 +1,3 @@
+final: prev: {
+  superset-granian = final.superset.passthru.granian;
+}
---
+++ overlays/superset.nix
@@ -0,0 +1,21 @@
+final: prev: let
+  supersetBase = prev.python3.pkgs.callPackage ./superset/default.nix {};
+  supersetEnv = prev.python3.withPackages (ps: [
+    supersetBase
+    ps.granian
+  ]);
+in {
+  superset = supersetBase.overrideAttrs (old: {
+    passthru =
+      (old.passthru or {})
+      // {
+        granian =
+          (prev.writeShellScriptBin "superset-granian" ''
+            export PATH=${supersetEnv}/bin:$PATH
+            exec ${supersetEnv}/bin/python -m granian --factory --interface wsgi "superset.app:create_app" "$@"
+          '').overrideAttrs (old: {
+            version = supersetBase.version;
+          });
+      };
+  });
+}
