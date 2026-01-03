7f5e4295a719dfcac6e0692eec4a5aa078f29c0a
ahirner <a.hirner@gmail.com>
2026-01-03 01:12:47 +0100

    try spiceai 1.10.3+

--- overlays/spiced.nix
+++ overlays/spiced.nix
@@ -3,29 +3,28 @@ final: prev: let
 in {
   spiced = rustPlatform.buildRustPackage {
     pname = "spiced";
-    version = "1.5-rc";
+    version = "1.10-rc";
 
     src = fetchFromGitHub {
       owner = "ahirner";
       repo = "spiceai";
-      rev = "chore/trunk_fixes";
-      hash = "sha256-ZdQzpRX+QUEUNw+93JT1gck9k7aHCbVNRoA0PvfB6qg=";
+      rev = "feature/spice_1.10.3-rc";
+      hash = "sha256-oSSugt9pa0rs8e4wecDHiaCVTGqznP45KwGHZ0SDT4E=";
     };
-    useFetchCargoVendor = true;
-    cargoHash = "sha256-OWKGprPjrBwHLB/k85GUvz21+S/8Tr2iEVw56EXAHok=";
+    cargoHash = "sha256-PvDuW1KBvvsC1wf3NfxEOXP3qEQGANZvNRK0gbazpYA=";
 
     buildNoDefaultFeatures = true;
     buildFeatures = ["flightsql" "postgres" "sqlite" "release"];
 
     env = {OPENSSL_NO_VENDOR = 1;};
-    nativeBuildInputs = with prev; [pkg-config cmake installShellFiles rustPlatform.bindgenHook];
+    nativeBuildInputs = with prev; [pkg-config cmake protobuf installShellFiles rustPlatform.bindgenHook];
     buildInputs = with prev;
-      lib.optionals (! stdenv.hostPlatform.isDarwin) [openssl]
+      lib.optionals (! stdenv.hostPlatform.isDarwin) [openssl.dev]
       ++ lib.optionals stdenv.hostPlatform.isDarwin [
         apple-sdk
         # aws-lc-sys requires CryptoKit's CommonCrypto, which is available on macOS 10.15+
         # c.f: https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/co/conduwuit/package.nix
-        (darwinMinVersionHook "10.15")
+        #(darwinMinVersionHook "10.15")
       ];
 
     # too much hassle
