89d1d0c011c466d97098dc6527283d54a71cca96
ahirner <a.hirner@gmail.com>
2026-01-09 22:35:53 +0100

    rm include_str of README outside nix sandboxed cargoDeps
    ../${CARGO_PKG_README} doesn't exist for rustPlatform.buildRustPackage with fetchCargoVendor
    
    https://github.com/rust-lang/cargo/pull/11645#issuecomment-1536905941

--- overlays/spiced.nix
+++ overlays/spiced.nix
@@ -1,17 +1,36 @@
 final: prev: let
-  inherit (prev) lib fetchFromGitHub stdenv rustPlatform;
+  inherit (prev) lib fetchFromGitHub stdenv rustPlatform runCommand;
+  src = fetchFromGitHub {
+    owner = "ahirner";
+    repo = "spiceai";
+    rev = "feature/spice_1.10.3-rc";
+    hash = "sha256-oSSugt9pa0rs8e4wecDHiaCVTGqznP45KwGHZ0SDT4E=";
+  };
+
+  cleanCargoDeps = rustPlatform.fetchCargoVendor {
+    inherit src;
+    name = "spiced-1.10-rc-vendor";
+    hash = "sha256-PvDuW1KBvvsC1wf3NfxEOXP3qEQGANZvNRK0gbazpYA=";
+  };
+
+  patchedCargoDeps =
+    runCommand "spiced-vendor-patched" {
+      inherit cleanCargoDeps;
+    } ''
+      mkdir -p $out
+      find $cleanCargoDeps -maxdepth 1 -mindepth 1 -exec ln -s {} $out/ \;
+      rm $out/vortex-0.1.0
+      cp -r $cleanCargoDeps/vortex-0.1.0 $out/
+      chmod -R +w $out/vortex-0.1.0
+      sed -i 's/^#!\[doc = include_str!/\/\/ &/' $out/vortex-0.1.0/src/lib.rs
+    '';
 in {
   spiced = rustPlatform.buildRustPackage {
+    inherit src;
     pname = "spiced";
     version = "1.10-rc";
 
-    src = fetchFromGitHub {
-      owner = "ahirner";
-      repo = "spiceai";
-      rev = "feature/spice_1.10.3-rc";
-      hash = "sha256-oSSugt9pa0rs8e4wecDHiaCVTGqznP45KwGHZ0SDT4E=";
-    };
-    cargoHash = "sha256-PvDuW1KBvvsC1wf3NfxEOXP3qEQGANZvNRK0gbazpYA=";
+    cargoDeps = patchedCargoDeps;
 
     buildNoDefaultFeatures = true;
     buildFeatures = ["flightsql" "postgres" "sqlite" "release"];
@@ -36,5 +55,7 @@ in {
       license = lib.licenses.asl20;
       maintainers = [];
     };
+
+    passthru.patchedCargoDeps = patchedCargoDeps;
   };
 }
