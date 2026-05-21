aa30a3605270b5708acb3df9c89a4e2c1ad26cde
ahirner <a.hirner@gmail.com>
2026-05-21 22:13:20 +0200

    superset 6.1.0

--- overlays/superset/dependencies.nix
+++ overlays/superset/dependencies.nix
@@ -3,6 +3,92 @@
   fetchPypi,
 }: rec {
   # Custom overrides
+  "apache-superset-core" = python.pkgs.buildPythonPackage rec {
+    pname = "apache-superset-core";
+    version = "0.1.0";
+    src = fetchPypi {
+      pname = "apache_superset_core";
+      inherit version;
+      hash = "sha256-cs3axVfyEtsw9xjvB0Glm8X1tUVZUl+aEE2cklq6GME=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python.pkgs; [
+      flask-appbuilder
+      pydantic
+      sqlalchemy_1_4
+      sqlalchemy-utils
+      sqlglot
+      typing-extensions
+    ];
+  };
+
+  sqlglot = python.pkgs.buildPythonPackage rec {
+    pname = "sqlglot";
+    version = "28.10.0";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-89R1kWSthUF2mAs6R+sMfvaZEY36gL7rk+AQiFY3shE=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools setuptools-scm];
+    doCheck = false;
+  };
+
+  marshmallow-union = python.pkgs.buildPythonPackage rec {
+    pname = "marshmallow-union";
+    version = "0.1.15.post1";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-wI8Kh4ka5z3StdShVLx9rqIMO8D5nKC2omVwySfSDIw=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python.pkgs; [marshmallow];
+  };
+
+  pygeohash = python.pkgs.buildPythonPackage rec {
+    pname = "pygeohash";
+    version = "3.2.2";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-qsUHH5qQIyxQ2MahJKFQ/9twBpBTFPcqENaGoH0sUYc=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools wheel];
+    doCheck = false;
+  };
+
+  redis = python.pkgs.buildPythonPackage rec {
+    pname = "redis";
+    version = "5.3.1";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-yklXelMepkA5taNts9bNGgx6YMNBJNRpJKRblW6M8Uw=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python.pkgs; [pyjwt];
+  };
+
+  # nixpkgs has 6.0.1, but its generated dist-info reports 0.0.1.
+  flask-cors = python.pkgs.buildPythonPackage rec {
+    pname = "flask-cors";
+    version = "6.0.1";
+    src = fetchPypi {
+      pname = "flask_cors";
+      inherit version;
+      hash = "sha256-2BvLMfB7CYW+f0hAYkfpJDrO0im3dHIZFgoFWe3WeNs=";
+    };
+    pyproject = true;
+    build-system = with python.pkgs; [setuptools wheel];
+    doCheck = false;
+    dependencies = with python.pkgs; [flask werkzeug];
+  };
+
   sqlalchemy-utils = python.pkgs.buildPythonPackage rec {
     pname = "SQLAlchemy-Utils";
     version = "0.42.1";
@@ -90,20 +176,6 @@
     pyproject = true;
     build-system = with python.pkgs; [flit-core];
     doCheck = false;
-  };
-
-  python-geohash = python.pkgs.buildPythonPackage rec {
-    pname = "python-geohash";
-    version = "0.8.5";
-    src = fetchPypi {
-      pname = "python-geohash";
-      inherit version;
-      hash = "sha256-BaIfz07aGl7dvSkYkK3iP8Xdqmu5jy7iPS04TtFPCG0=";
-    };
-    pyproject = true;
-    build-system = with python.pkgs; [setuptools wheel];
-    doCheck = false;
-    dependencies = with python.pkgs; [setuptools];
   };
 
   shillelagh = python.pkgs.buildPythonPackage rec {
@@ -210,19 +282,21 @@
       ]);
   };
 
+  # Not required by Superset core; included for the Flight SQL connector support
+  # advertised by this overlay.
   flightsql-dbapi = python.pkgs.buildPythonPackage rec {
     pname = "flightsql-dbapi";
     version = "0.2.2";
     src = fetchPypi {
       pname = "flightsql_dbapi";
       inherit version;
-      sha256 = "b6de4d81fa31e7357507c3d493be31c4e68672dd1b4a2a4554463ac4450e5851";
+      hash = "sha256-tt5Ngfox5zV1B8PUk74xxOaGct0bSipFVEY6xEUOWFE=";
     };
     pyproject = true;
     build-system = with python.pkgs; [hatchling];
     postPatch = ''
-            substituteInPlace pyproject.toml --replace-fail "hatchling<=1.18.0" "hatchling"
-            cat >> pyproject.toml <<EOF
+      substituteInPlace pyproject.toml --replace-fail "hatchling<=1.18.0" "hatchling"
+      cat >> pyproject.toml <<EOF
 
       [tool.hatch.build.targets.wheel]
       packages = ["flightsql"]
